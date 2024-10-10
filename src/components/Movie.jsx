f38c96e00e03894d6f005f93bb2171805b4811f4
sky4564 <qw486512@gmail.com>
2024-10-11 01:15:48 +0900

    FEAT: add routing

---
+++ src/components/Movie.jsx
@@ -0,0 +1,23 @@
+import propTypes from 'prop-types'
+
+Movie.propTypes = {
+  coverImg: propTypes.string.isRequired,
+  title: propTypes.string.isRequired,
+  summary: propTypes.string.isRequired,
+  genres: propTypes.arrayOf(propTypes.string).isRequired,
+}
+
+export default function Movie({ coverImg, title, summary, genres }) {
+  return (
+    <div>
+      <img src={coverImg}></img>
+      <h2>{title}</h2>
+      <p>{summary}</p>
+      <ul>
+        {genres.map((g) => (
+          <li key={g}>{g}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
---
+++ src/routes/Detail.jsx
@@ -0,0 +1,5 @@
+export default function Detail() {
+    return (
+        <h1> is detail ! </h1>
+    )
+}
