295b4f08fad905ef5ea68679121332a3bc0b97ec
sky4564 <qw486512@gmail.com>
2024-10-10 23:58:48 +0900

    first commit

---
+++ .history/src/App_20241010233404.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'react'
+
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+      )
+    )
+  }
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
