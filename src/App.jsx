30e8ee4ebf93c0985afd37761faa4c689defcdb8
sky4564 <qw486512@gmail.com>
2024-10-11 13:48:51 +0900

    gh page 빌드설정

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Detail from "./routes/Detail";
 import Home from "./routes/Home";
 
+const PUBLC_URL = import.meta.env.VITE_PUBLC_URL
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/hello" element={<h1>Hello</h1>} />
+
         <Route path="/movie/:id" element={<Detail />} />
         <Route path="/" element={<Home />} />
+
+        <Route path={`${PUBLC_URL}/movie/:id`} element={<Detail />} />
+        <Route path={`${PUBLC_URL}/`} element={< Home />} />
       </Routes>
     </BrowserRouter>
   );
